a14860566012bb3c74606c3f7f58cd49751f142e
fernandovmedina <fernandovazquez.favm@gmail.com>
2025-09-03 00:27:10 -0600

    added assets and fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,35 @@
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 import '../global.css';
 
 export default function RootLayout() {
-  /*
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    // Roboto normal
+    RobotoBold: require('../assets/fonts/roboto/normal/bold.ttf'),
+    RobotoExtralight: require('../assets/fonts/roboto/normal/extralight.ttf'),
+    RobotoLight: require('../assets/fonts/roboto/normal/light.ttf'),
+    RobotoMedium: require('../assets/fonts/roboto/normal/medium.ttf'),
+    RobotoRegular: require('../assets/fonts/roboto/normal/regular.ttf'),
+    RobotoSemibold: require('../assets/fonts/roboto/normal/semibold.ttf'),
+    RobotoThin: require('../assets/fonts/roboto/normal/thin.ttf'),
+    // Roboto italic
+    RobotoItalicBlack: require('../assets/fonts/roboto/italic/black-italic.ttf'),
+    RobotoItalicBold: require('../assets/fonts/roboto/italic/bold-italic.ttf'),
+    RobotoItalicExtrabold: require('../assets/fonts/roboto/italic/extrabold-italic.ttf'),
+    RobotoItalicExtralight: require('../assets/fonts/roboto/italic/extralight-italic.ttf'),
+    RobotoItalicRegular: require('../assets/fonts/roboto/italic/italic.ttf'),
+    RobotoItalicLight: require('../assets/fonts/roboto/italic/light-italic.ttf'),
+    RobotoItalicMedium: require('../assets/fonts/roboto/italic/medium-italic.ttf'),
+    RobotoItalicSemibold: require('../assets/fonts/roboto/italic/semibold-italic.ttf'),
+    RobotoItalicThin: require('../assets/fonts/roboto/italic/thin-italic.ttf'),
   });
 
   if (!loaded) {
-    // Async font loading only occurs in development.
     return null;
   }
-  */
 
   return (
     <ThemeProvider value={DefaultTheme}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // Roboto normal
+        roboto: ["RobotoRegular", "sans-serif"],
+        "roboto-bold": ["RobotoBold","sans-serif"],
+        "roboto-extralight": ["RobotoExtralight","sans-serif"],
+        "roboto-light": ["RobotoLight","sans-serif"],
+        "roboto-medium": ["RobotoMedium","sans-serif"],
+        "roboto-semibold": ["RobotoSemibold","sans-serif"],
+        "roboto-thin": ["RobotoThin","sans-serif"],
+        // Roboto italic
+        "roboto-italic-black": ["RobotoItalicBlack","sans-serif"],
+        "roboto-italic-bold": ["RobotoItalicBold","sans-serif"],
+        "roboto-italic-extrabold": ["RobotoItalicExtrabold","sans-serif"],
+        "roboto-italic-extralight": ["RobotoItalicExtralight","sans-serif"],
+        "roboto-italic-regular": ["RobotoItalicRegular","sans-serif"],
+        "roboto-italic-light": ["RobotoItalicLight","sans-serif"],
+        "roboto-italic-medium": ["RobotoItalicMedium","sans-serif"],
+        "roboto-italic-semibold": ["RobotoItalicSemibold","sans-serif"],
+        "roboto-italic-thin": ["RobotoItalicThin","sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
